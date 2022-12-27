a059debb4d9c2b0b788780a18339ecdf417cc007
GuotongWu <wuguotongswjtu@gmail.com>
2022-12-27 09:59:10 +0800

    commit before add noisy

--- ECAPAModel.py
+++ ECAPAModel.py
@@ -35,67 +35,88 @@ class ECAPAModel(nn.Module):
 			index += len(labels)
 			top1 += prec
 			loss += nloss.detach().cpu().numpy()
-			sys.stderr.write(time.strftime("%m-%d %H:%M:%S") + \
+			sys.stderr.write("Train: " + time.strftime("%m-%d %H:%M:%S") + \
 			" [%2d] Lr: %5f, Training: %.2f%%, "    %(epoch, lr, 100 * (num / loader.__len__())) + \
 			" Loss: %.5f, ACC: %2.2f%% \r"        %(loss/(num), top1/index*len(labels)))
 			sys.stderr.flush()
 		sys.stdout.write("\n")
-		# self.scheduler.step()
+		self.scheduler.step()
 		return loss/num, lr, top1/index*len(labels)
 
-	def eval_network(self, eval_list, eval_path):
+
+	def eval_network(self, epoch, loader):
 		self.eval()
-		files = []
-		embeddings = {}
-		lines = open(eval_list).read().splitlines()
-		for line in lines:
-			files.append(line.split()[1])
-			files.append(line.split()[2])
-		setfiles = list(set(files))
-		setfiles.sort()
+		with torch.no_grad():
+			index, top1, loss = 0, 0, 0
+			for num, (data, labels) in enumerate(loader, start = 1):
+				labels            = labels.cuda()
+				speaker_embedding = self.speaker_encoder.forward(data.cuda(), aug = False)
+				nloss, prec       = self.speaker_loss.forward(speaker_embedding, labels)
+				index += len(labels)
+				top1 += prec
+				loss += nloss.detach().cpu().numpy()
+				sys.stderr.write("Valid: " + time.strftime("%m-%d %H:%M:%S") + \
+				" [%2d] Testing: %.2f%%, "    %(epoch, 100 * (num / loader.__len__())) + \
+				" Loss: %.5f, ACC: %2.2f%% \r"        %(loss/(num), top1/index*len(labels)))
+				sys.stderr.flush()
+			sys.stdout.write("\n")
 
-		for idx, file in tqdm.tqdm(enumerate(setfiles), total = len(setfiles)):
-			audio, _  = soundfile.read(os.path.join(eval_path, file))
-			# Full utterance
-			data_1 = torch.FloatTensor(numpy.stack([audio],axis=0)).cuda()
+		return loss/num, top1/index*len(labels)
 
-			# Spliited utterance matrix
-			max_audio = 300 * 160 + 240
-			if audio.shape[0] <= max_audio:
-				shortage = max_audio - audio.shape[0]
-				audio = numpy.pad(audio, (0, shortage), 'wrap')
-			feats = []
-			startframe = numpy.linspace(0, audio.shape[0]-max_audio, num=5)
-			for asf in startframe:
-				feats.append(audio[int(asf):int(asf)+max_audio])
-			feats = numpy.stack(feats, axis = 0).astype(numpy.float)
-			data_2 = torch.FloatTensor(feats).cuda()
-			# Speaker embeddings
-			with torch.no_grad():
-				embedding_1 = self.speaker_encoder.forward(data_1, aug = False)
-				embedding_1 = F.normalize(embedding_1, p=2, dim=1)
-				embedding_2 = self.speaker_encoder.forward(data_2, aug = False)
-				embedding_2 = F.normalize(embedding_2, p=2, dim=1)
-			embeddings[file] = [embedding_1, embedding_2]
-		scores, labels  = [], []
 
-		for line in lines:			
-			embedding_11, embedding_12 = embeddings[line.split()[1]]
-			embedding_21, embedding_22 = embeddings[line.split()[2]]
-			# Compute the scores
-			score_1 = torch.mean(torch.matmul(embedding_11, embedding_21.T)) # higher is positive
-			score_2 = torch.mean(torch.matmul(embedding_12, embedding_22.T))
-			score = (score_1 + score_2) / 2
-			score = score.detach().cpu().numpy()
-			scores.append(score)
-			labels.append(int(line.split()[0]))
+	# def eval_network(self, eval_list, eval_path):
+	# 	self.eval()
+	# 	files = []
+	# 	embeddings = {}
+	# 	lines = open(eval_list).read().splitlines()
+	# 	for line in lines:
+	# 		files.append(line.split()[1])
+	# 		files.append(line.split()[2])
+	# 	setfiles = list(set(files))
+	# 	setfiles.sort()
+
+	# 	for idx, file in tqdm.tqdm(enumerate(setfiles), total = len(setfiles)):
+	# 		audio, _  = soundfile.read(os.path.join(eval_path, file))
+	# 		# Full utterance
+	# 		data_1 = torch.FloatTensor(numpy.stack([audio],axis=0)).cuda()
+
+	# 		# Spliited utterance matrix
+	# 		max_audio = 300 * 160 + 240
+	# 		if audio.shape[0] <= max_audio:
+	# 			shortage = max_audio - audio.shape[0]
+	# 			audio = numpy.pad(audio, (0, shortage), 'wrap')
+	# 		feats = []
+	# 		startframe = numpy.linspace(0, audio.shape[0]-max_audio, num=5)
+	# 		for asf in startframe:
+	# 			feats.append(audio[int(asf):int(asf)+max_audio])
+	# 		feats = numpy.stack(feats, axis = 0).astype(numpy.float)
+	# 		data_2 = torch.FloatTensor(feats).cuda()
+	# 		# Speaker embeddings
+	# 		with torch.no_grad():
+	# 			embedding_1 = self.speaker_encoder.forward(data_1, aug = False)
+	# 			embedding_1 = F.normalize(embedding_1, p=2, dim=1)
+	# 			embedding_2 = self.speaker_encoder.forward(data_2, aug = False)
+	# 			embedding_2 = F.normalize(embedding_2, p=2, dim=1)
+	# 		embeddings[file] = [embedding_1, embedding_2]
+	# 	scores, labels  = [], []
+
+	# 	for line in lines:			
+	# 		embedding_11, embedding_12 = embeddings[line.split()[1]]
+	# 		embedding_21, embedding_22 = embeddings[line.split()[2]]
+	# 		# Compute the scores
+	# 		score_1 = torch.mean(torch.matmul(embedding_11, embedding_21.T)) # higher is positive
+	# 		score_2 = torch.mean(torch.matmul(embedding_12, embedding_22.T))
+	# 		score = (score_1 + score_2) / 2
+	# 		score = score.detach().cpu().numpy()
+	# 		scores.append(score)
+	# 		labels.append(int(line.split()[0]))
 			
-		# Coumpute EER and minDCF
-		EER = tuneThresholdfromScore(scores, labels, [1, 0.1])[1]
-		fnrs, fprs, thresholds = ComputeErrorRates(scores, labels)
-		minDCF, _ = ComputeMinDcf(fnrs, fprs, thresholds, 0.05, 1, 1)
+	# 	# Coumpute EER and minDCF
+	# 	EER = tuneThresholdfromScore(scores, labels, [1, 0.1])[1]
+	# 	fnrs, fprs, thresholds = ComputeErrorRates(scores, labels)
+	# 	minDCF, _ = ComputeMinDcf(fnrs, fprs, thresholds, 0.05, 1, 1)
 
-		return EER, minDCF
+	# 	return EER, minDCF
 
 	def save_parameters(self, path):
 		torch.save(self.state_dict(), path)
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def set_seed(seed):
 
 set_seed(0)
 
-s = ECAPAModel(lr=0.002, lr_decay=0.97, C=1024, n_class=251, m=0.2, s=30, test_step=1)
+s = ECAPAModel(lr=0.002, lr_decay=0.95, C=1024, n_class=251, m=0.2, s=30, test_step=1)
 train_data = SoundTrainValidDataset("./LibriSpeech-SI/train", "train", segement_length=16000*2+240)
 valid_data = SoundTrainValidDataset("./LibriSpeech-SI/train", "valid", segement_length=16000*2+240)
 train_loader = DataLoader(dataset=train_data, batch_size=128, shuffle=True, num_workers=4)
@@ -22,3 +22,7 @@ valid_loader = DataLoader(dataset=valid_data, batch_size=128, shuffle=False, num
 
 for epoch in range(100):
     s.train_network(epoch, train_loader)
+
+    if epoch % 1 == 0:
+        s.save_parameters("./saved_model"+"/model_%04d.model"%epoch)
+        s.eval_network(epoch, valid_loader)
